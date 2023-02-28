7d26b954d5e09335c514b77cc4227d06633e2dc2
vasilkrug <vkruglovec@gmail.com>
2023-02-28 19:24:13 +0300

    test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/header/Header';
 import HeroSection from './components/heroSection/HeroSection';
 
 function App() {
+    //test
     return (
         <div className={'App'}>
             <Header/>
