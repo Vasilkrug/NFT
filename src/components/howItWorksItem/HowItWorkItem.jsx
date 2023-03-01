ac6d4140b8202220e08e761756b03ce3900ecbe5
vasilkrug <vkruglovec@gmail.com>
2023-03-01 22:48:32 +0300

    add component howItWorkItem

---
+++ src/components/howItWorksItem/HowItWorkItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HowItWorkItem.scss';
+
+const HowItWorkItem = ({title,image,text}) => {
+    return (
+        <li className={'how-it-work-card'}>
+            <img src={image} alt="info-image"/>
+            <div className={'how-it-work-info'}>
+                <h4>{title}</h4>
+                <p>{text}</p>
+            </div>
+        </li>
+    );
+};
+
+export default HowItWorkItem;
