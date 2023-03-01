c5c088dcc94a000a8caa0db0119db08a18ff9a54
vasilkrug <vkruglovec@gmail.com>
2023-03-01 22:49:18 +0300

    add component TrendingItem

---
+++ src/components/trendingItem/TrendingItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './TrendingItem.scss';
+
+const TrendingItem = ({title, images, userIcon, user}) => {
+    return (
+        <li className={'collection-card'}>
+            <div className={'collection-card-images'}>
+                {images.map(image => {
+                    return <div key={image} className={'card-image'}>
+                        <img src={image} alt=""/>
+                    </div>
+                })}
+            </div>
+            <div className="collection-card-user">
+                <p>{title}</p>
+                <img src={userIcon} alt="user-icon"/>
+                <span>{user}</span>
+            </div>
+        </li>
+    );
+};
+
+export default TrendingItem;
