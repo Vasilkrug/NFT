ac2bb48ea1946b59b371e925119e9db9e2c10611
vasilkrug <vkruglovec@gmail.com>
2023-03-01 22:46:34 +0300

    add component topCreatorList

---
+++ src/components/topCreatorsList/topCreatorsList.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './topCreatorsList.scss';
+import SectionTitle from "../sectionTitle/SectionTitle";
+import CustomButton from "../UI/customButton/CustomButton";
+import {rocket} from "../../assets/icons/icons";
+import {creators} from "./creatorsData";
+import CreatorItem from "../creatorItem/CreatorItem";
+
+const TopCreatorsList = () => {
+    return (
+        <section className={'top-creators'}>
+            <div className={'top-creators-title'}>
+                <SectionTitle title={'Top creators'} text={'Checkout Top Rated Creators on the NFT Marketplace'}/>
+                <CustomButton
+                    text={'View Rankings'}
+                    icon={rocket}
+                    style={{backgroundColor: '#2B2B2B', border: '2px solid #A259FF', padding: '20px 50px'}}/>
+            </div>
+            <ul className={'creators-list'}>
+                {creators.map((user, index) => {
+                    return <CreatorItem
+                        key={user.id}
+                        user={user.user}
+                        icon={user.icon}
+                        sales={user.sales}
+                        id={user.id}
+                        rank={index + 1}/>
+                })}
+            </ul>
+        </section>
+    );
+};
+
+export default TopCreatorsList;
