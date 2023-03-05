2baaa90732386abb9aba7f1e56699941e5431ec0
vasilkrug <vkruglovec@gmail.com>
2023-03-05 16:35:50 +0300

    add component Table

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Table.scss';
+import {creators} from "../topCreatorsList/creatorsData";
+
+const Table = () => {
+    return (
+        <table className={'table'}>
+            <thead>
+            <tr>
+                <th>*</th>
+                <th>Artist</th>
+                <th>Change</th>
+                <th>NFTs Sold</th>
+                <th>Volume</th>
+            </tr>
+            </thead>
+            <tbody>
+            {creators.map((user, index) => {
+                return <tr>
+                    <td className={'user-count'}><span>{index + 1}</span></td>
+                    <td className={'user-info'}>
+                        <img src={user.icon} alt=""/>
+                        <span>{user.user}</span>
+                    </td>
+                    <td className={'nft-change'}>+1.41%</td>
+                    <td>602</td>
+                    <td>12.4 ETH</td>
+                </tr>
+            })}
+            </tbody>
+        </table>
+    );
+};
+
+export default Table;
